9c6ac05ab472dcc082e5fabe10dfe20ce1d22844
Kittikun Singkhamma <s.kittikun3@gmail.com>
2022-10-22 17:08:47 +0700

    22/10/22

--- controller/users-control.js
+++ controller/users-control.js
@@ -1,4 +1,8 @@
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const secret = 'Jknowledge-login-nodejs-login-21-10-22'
 
 const db = mysql.createConnection({
     user: "root",
@@ -12,33 +16,55 @@ const createUser = (req, res) => {
     const password = req.body.password;
     const access_level = req.body.access_level;
 
-    db.query("INSERT INTO j_users (tel, password, access_level) VALUES (?,?,?)",
-        [tel, password, access_level],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send("Inserted")
+    bcrypt.hash(password, saltRounds, (err, hashPassword) => {
+        db.query("INSERT INTO j_users (tel, password, access_level) VALUES (?,?,?)",
+            [tel, hashPassword, access_level],
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.send('register successfully.')
+                }
             }
-        }
 
-    );
+        );
+    });
 };
+
 const Signin = (req, res) => {
     const tel = req.body.tel;
     const password = req.body.password;
 
-    db.query("SELECT * FROM j_users WHERE tel= ? AND password= ?", [tel, password],
+    db.query("SELECT * FROM j_users WHERE tel= ?", [tel],
         (err, result) => {
             if (err) {
-                res.send({ err: err });
+                res.json({ status: 'error', message: err });
+            }
+            if (result.length == 0) {
+                res.json({ status: 'error', message: 'ไม่พบเบอร์โทรศัพท์!' })
             }
             if (result.length > 0) {
-                res.send(result);
-            } else {
-                res.send({ message: "Something Wrong, Check Your tel and password" })
+                bcrypt.compare(password, result[0].password, (err, isLogin) => {
+                    if (isLogin) {
+                        const token = jwt.sign({ tel: result[0].tel }, secret);
+                        res.json({ status: 'ok', message: result, token })
+                    } else {
+                        res.json({ status: 'error', message: 'รหัสผ่านไม่ถูกต้อง' })
+                    }
+                });
             }
         })
+}
+
+const authToken = (req, res) => {
+    try {
+        const token = req.body.token
+        console.log(token);
+        const decoded = jwt.verify(token, secret);
+        res.send({ status: 'ok', decoded })
+    } catch (err) {
+        res.send({ status: 'error', message: err.message })
+    }
 }
 
 const getUsers = (req, res) => {
@@ -67,7 +93,6 @@ const updateUser = (req, res) => {
 
 const deleteUser = (req, res) => {
     const id = req.params.id;
-    console.log(id);
     db.query("DELETE FROM j_users WHERE id = ?", id, (err, result) => {
         if (err) {
             console.log(err);
@@ -82,5 +107,6 @@ module.exports = {
     createUser,
     updateUser,
     deleteUser,
-    Signin
+    Signin,
+    authToken
 };
--- routes/users-Route.js
+++ routes/users-Route.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 
 const { getUsers,
     createUser,
     updateUser,
     deleteUser,
-    Signin } = require('../controller/users-control')
+    Signin,
+    authToken } = require('../controller/users-control')
 
 const userRoute = express.Router()
 
 userRoute.get('/users', getUsers)
-userRoute.post('/create-user', createUser)
+userRoute.post('/create-user',jsonParser, createUser)
 userRoute.put('/update', updateUser)
 userRoute.delete('/delete/:id', deleteUser)
-userRoute.post('/signin', Signin)
+userRoute.post('/signin',jsonParser, Signin)
+userRoute.post('/authsignin',jsonParser, authToken)
 
 
 module.exports = userRoute
